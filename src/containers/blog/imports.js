f3b778e9b96f5b5e5e1e6c2069a78e0ad72f7ff3
Arzu-Shikhieva <arzum.shixieffa@mail.ru>
2021-11-13 21:47:42 +0400

    initial commit

---
+++ src/containers/blog/imports.js
@@ -0,0 +1,13 @@
+import article from '../../assets/article-1.png';
+import articleTwo from '../../assets/article-2.png';
+import articleThree from '../../assets/article-3.png';
+import articleFour from '../../assets/article-4.png';
+import articleFive from '../../assets/article-5.png';
+
+export {
+    article,
+    articleTwo,
+    articleThree,
+    articleFour,
+    articleFive,
+};
